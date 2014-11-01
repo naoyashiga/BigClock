b06d452636b253fc2515ef4ebdd1a8380da92a8c
naoyashiga <naoyashiga0@gmail.com>
2014-11-02 08:36:50 +0900

    Initial Commit

---
+++ BigClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BigClock
+//
+//  Created by naoyashiga on 2014/11/02.
+//  Copyright (c) 2014年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
